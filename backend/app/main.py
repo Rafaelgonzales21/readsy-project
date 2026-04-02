b9a4ee958e39cbde611ffe4d226663066d7eb6ae
Rafael <gonzalesstalin05@gmail.com>
2026-04-02 15:11:34 +0200

    feat: initialize FastAPI backend project

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+# Punto de entrada del backend FastAPI
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.routes_analysis import router as analysis_router
+
+app = FastAPI(title="Academic Analyzer API")
+
+# Configuración CORS - Sirve para que React se conecte con el backend
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Registrar las rutas
+
+app.include_router(analysis_router, prefix="/api")
